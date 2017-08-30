88e7fc31125ea33ba0ad47a38de1d5cd56f5ce5c
Adam Bujalski <a.bujalski@samsung.com>
2017-08-30 14:51:06 +0900

    VDGONACL-394: Removal of building probrams for libcurl
    Change-Id: Ib31c96b15f622a789f5f3a43c2dfc4b694a70783

--- ports/curl/build.sh
+++ ports/curl/build.sh
@@ -12,8 +12,6 @@ export EXTRA_LIBS="${NACL_CLI_MAIN_LIB} -l${NACL_CXX_LIB}"
 
 EnableGlibcCompat
 
-EXECUTABLES="src/curl${NACL_EXEEXT}"
-
 EXTRA_CONFIGURE_ARGS+=" --without-libssh2"
 if [ "${NACL_DEBUG}" = "1" ]; then
   NACLPORTS_CPPFLAGS+=" -DDEBUGBUILD"
@@ -34,31 +32,9 @@ PublishStep() {
     EXECUTABLE_DIR=.
   fi
 
-  if [ "${NACL_ARCH}" = "pnacl" ]; then
-    # Just set up the x86-64 version for now.
-    local pexe="${EXECUTABLE_DIR}/curl${NACL_EXEEXT}"
-    (cd src;
-     TranslateAndWriteLauncherScript ${pexe} x86-64 curl.x86-64.nexe curl
-    )
-  fi
-
   MakeDir ${PUBLISH_DIR}
 
-  local exe=${PUBLISH_DIR}/curl_${NACL_ARCH}${NACL_EXEEXT}
-
   LogExecute cp ${DESTDIR}${PREFIX}/share/curl/ca-bundle.crt ${PUBLISH_DIR}
-  LogExecute cp src/${EXECUTABLE_DIR}/curl${NACL_EXEEXT} ${exe}
-  if [ "${NACL_ARCH}" = "pnacl" ]; then
-    LogExecute ${PNACLFINALIZE} ${exe}
-  fi
-
-  pushd ${PUBLISH_DIR}
-  LogExecute python ${NACL_SDK_ROOT}/tools/create_nmf.py \
-      ${PUBLISH_DIR}/curl*${NACL_EXEEXT} \
-      -L${DESTDIR_LIB} \
-      -s . \
-      -o curl.nmf
-  popd
 
   InstallNaClTerm ${PUBLISH_DIR}
   LogExecute cp ${START_DIR}/index.html ${PUBLISH_DIR}
